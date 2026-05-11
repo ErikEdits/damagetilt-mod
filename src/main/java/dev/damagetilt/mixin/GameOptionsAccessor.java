c8a6257d5b65512bd3db37a7e594e1d91f5f828a
ErikEdits <erikcool257@gmail.com>
2026-05-11 22:24:17 +0200

    Refactor GameRendererMixin to GameOptionsAccessor

---
+++ src/main/java/dev/damagetilt/mixin/GameOptionsAccessor.java
@@ -0,0 +1,12 @@
+package dev.damagetilt.mixin;
+
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.option.SimpleOption;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(GameOptions.class)
+public interface GameOptionsAccessor {
+    @Accessor("damageTiltStrength")
+    SimpleOption<Double> getDamageTiltStrength();
+}
